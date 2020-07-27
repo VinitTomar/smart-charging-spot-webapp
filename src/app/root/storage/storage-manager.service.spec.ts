9ec450a7a69e2b89d37c4f0a72a790bab19ebf60
Vinit Tomar <vinit.tomar68@gmail.com>
2020-07-27 22:07:43 +0530

    Storage manager UT done

--- src/app/root/storage/storage-manager.service.spec.ts
+++ src/app/root/storage/storage-manager.service.spec.ts
@@ -2,11 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { StorageManager } from './storage-manager.service';
 
-describe('StorageManager', () => {
+fdescribe('StorageManager', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: StorageManager = TestBed.get(StorageManager);
     expect(service).toBeTruthy();
+  });
+
+  it('should return localstoreage', () => {
+    const service: StorageManager = TestBed.get(StorageManager);
+    expect(service.storage).toBeTruthy();
   });
 });
