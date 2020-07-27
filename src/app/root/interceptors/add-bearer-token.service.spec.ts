45f629da82c6d0ce51664b4db78172f0c6f7ebf8
Vinit Tomar <vinit.tomar68@gmail.com>
2020-07-27 16:22:49 +0530

    add-bearer-token service unit testing done

--- src/app/root/interceptors/add-bearer-token.service.spec.ts
+++ src/app/root/interceptors/add-bearer-token.service.spec.ts
@@ -1,12 +1,60 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AddBearerToken } from './add-bearer-token.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe('AddBearerToken', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { AddBearerToken } from './add-bearer-token.service';
+import { UserService } from '../services';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+class UserServiceStub {
+  loggedIn = true;
+  token = 'fake token';
+}
+
+
+fdescribe('AddBearerToken', () => {
+  let httpClientMock: HttpClient;
+  let httpMock: HttpTestingController;
+  let userServiceStub: UserServiceStub;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [
+      { provide: UserService, useClass: UserServiceStub },
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AddBearerToken,
+        multi: true
+      }
+    ]
+  }));
+
+  beforeEach(() => {
+    httpClientMock = TestBed.get(HttpClient);
+    httpMock = TestBed.get(HttpTestingController);
+    userServiceStub = TestBed.get(UserService);
+  })
 
   it('should be created', () => {
     const service: AddBearerToken = TestBed.get(AddBearerToken);
     expect(service).toBeTruthy();
+  });
+
+  it('should add Authorization header, if user loggedIn', () => {
+    userServiceStub.loggedIn = true;
+    httpClientMock.get('dummy-url').subscribe(res => {
+      expect(res).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne('dummy-url');
+    expect(httpRequest.request.headers.has('Authorization')).toBeTruthy();
+  });
+
+  it('should not add Authorization header, if user is not loggedIn', () => {
+    userServiceStub.loggedIn = false;
+    httpClientMock.get('dummy-url').subscribe(res => {
+      expect(res).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne('dummy-url');
+    expect(httpRequest.request.headers.has('Authorization')).toBeFalsy();
   });
 });
